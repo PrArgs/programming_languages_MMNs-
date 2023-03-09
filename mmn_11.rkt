0434d9ac385d1ad3a363cb511097aee3a7495c4f
Roi Argaman <zebroi.arg@gmail.com>
2023-03-09 17:44:03 +0200

    q1a_complete

--- mmn_11.rkt
+++ mmn_11.rkt
@@ -1,17 +1,23 @@
 #lang scheme
 
-;this is the Work product of Roi Argaman
-;
-;Q1.a : My_append
-
+#|
+this is the Work product of Roi Argaman
+Q1.a : My_append 
+|#
 (define my_append
   (lambda (lst1 lst2)
-     (if (eqv? (length lst1) (length lst2))
-         (if (and(empty? lst1) (empty? lst2))
-             '()
-             (cons (car lst1) (cons (car lst2) (my_append (cdr lst1) (cdr lst2)))))
-             (display "Not a valid input"))))
+    (if (not (empty? lst1))
+        (cons (car lst1) (my_append (cdr lst1) lst2))
+        (if (empty? lst2)
+            '()
+            (cons (car lst2) (my_append  lst1 (cdr lst2)))
+            ))))
 
+
+
+        
+        
+ 
 ;Q1.b : My_append no recursion 
 
 
